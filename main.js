d4721038462890ae4734e64fe2dc96500fbdb06d
manjeetcoder2022 <99887565+manjeetcoder2022@users.noreply.github.com>
2023-02-18 13:20:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+
+function bed(){
+    window.location="bed.html"
+}
+
+function tv(){
+    window.location="tv.html"
+}
+function bottles(){
+    window.location="bottel.html"
+}
+
+function Fruit(){
+
+    window.location="fruit.html"
+}
+function desk(){
+    window.location="desk.html"
+}
+
+
+
+
+// function preload(){
+//     bed= loadImage("bed_room.jpg")
+//     tv= loadImage("tv.webp")
+//     desk= loadImage("desk.jpg")
+//     fruit= loadImage("fruit.jpg")
+//     bottels= loadImage("bottels.jpg")
+// }
+
+// function setup(){
+//     // canvas= createCanvas(800,800)
+// }
